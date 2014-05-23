376718b4656025b3881de9a64fdd994d4dd8fedd
Gavin Jaynes <gavin@logicspot.com>
2014-05-23 15:12:38 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+### 0.8: May 23th, 2014
+* Combined Roots 6.5.2 with Foundation 5
---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+<footer class="row content-info" role="contentinfo">
+   <?php dynamic_sidebar('sidebar-footer'); ?>
+   <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+</footer>
+
+<?php wp_footer(); ?>
